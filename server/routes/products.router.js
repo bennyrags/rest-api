2df1f3280f909d843ff60c159c8455815418e2bc
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-05-09 21:52:37 -0500

    router set up for products, nodemon added

--- server/routes/products.router.js
+++ server/routes/products.router.js
@@ -1,1 +1,16 @@
-module.exports = 'hello!';
+let express = require('express');
+let router = express.Router();
+let products = [];
+
+//get req takes in an id as an arg
+router.get('/:id', function(req, res){
+    
+    //router needs to get id
+    
+    let id = req.params.id;
+
+    res.send(id);
+  });
+  
+
+module.exports = router;
