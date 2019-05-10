ee044a655dd73d53244aa83def1a7e540f90a392
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-05-09 22:45:10 -0500

    successfully added axios req to router, connecting to external endpoint

--- server/routes/products.router.js
+++ server/routes/products.router.js
@@ -1,16 +1,35 @@
 let express = require('express');
 let router = express.Router();
 let products = [];
-
+let axios = require('axios');
 //get req takes in an id as an arg
 router.get('/:id', function(req, res){
-    
-    //router needs to get id
-    
+    //router needs to get id    
     let id = req.params.id;
+    
+    const getFromTarget = async => {
 
-    res.send(id);
-  });
+        try {
+            axios.get(`https://redsky.target.com/v2/pdp/tcin/${id}`)
+            .then(response=>{
+                res.send(response.data);
+            })
+            .catch(error=>{
+                console.log('error when getting this thang', error);
+                res.sendStatus(500);
+            })
+
+        }
+
+        catch (error) {
+            console.log('error when trying to make an axios get req in an async:', error)
+        }
+
+    }
+
+    getFromTarget();
+    
+});
   
 
 module.exports = router;
