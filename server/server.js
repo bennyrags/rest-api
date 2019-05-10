8cf8c781df32ae054589fe43fc9bd3d7b2cffe31
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-05-09 21:34:01 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const products = require('./routes/products.router.js');
+const PORT = 5000;
+
+/** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.json()); // needed for post/put requests
+//app.use(express.static('build'));
+app.use(express.static("server/public"));
+
+/** ---------- EXPRESS ROUTES ---------- **/
+app.use('/api/products', products);
+
+
+/** ---------- START SERVER ---------- **/
+app.listen(PORT,  () => {
+    console.log('Listening on port: ', PORT);
+});
